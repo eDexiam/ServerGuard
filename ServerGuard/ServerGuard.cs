9207a4dc784ef79ad32c105fcfbfda8714119f1e
lucasboss45 <l4f4go@gmail.com>
2019-06-02 02:36:46 +0200

    Updated version

--- ServerGuard/ServerGuard.cs
+++ ServerGuard/ServerGuard.cs
@@ -15,7 +15,7 @@ namespace ServerGuard
 		name = "ServerGuard",
 		description = "A banning system for troublemakers",
 		id = "lucas.serverguard.plugin",
-		version = "1.5.1",
+		version = "1.6",
 		SmodMajor = 3,
 		SmodMinor = 4,
 		SmodRevision = 0
